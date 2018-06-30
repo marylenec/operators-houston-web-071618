f2cc08b02114d810f911891b3898ebe07b9a895b
marylene.chan@gmail.com <marylene.chan@gmail.com>
2018-06-30 12:53:54 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,20 @@
 def unsafe?(speed)
-
+  if speed > 60 || speed < 40
+  true
+  else speed < 60 && speed > 40
+  false  
+  end  
 end
+
+unsafe?(50)
 
 
 
 def not_safe?(speed)
-	
+  speed > 60 || speed < 40 ? true : false
+  speed < 60 && speed > 40 ? false : true
 end
+
 	
 
 
