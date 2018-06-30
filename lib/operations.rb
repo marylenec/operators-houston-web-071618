e061d202905cd28899330c339fde8a9cbef91882
marylene.chan@gmail.com <marylene.chan@gmail.com>
2018-06-30 12:43:10 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -12,7 +12,7 @@ unsafe?(50)
 
 def not_safe?(speed)
   speed > 60 || speed < 40 ? true : false
-  speed > 60 && speed < 40 ? false : true
+  speed > 60 && speed > 40 ? false : true
 end
 
 not_safe?(59)	
